647dc7154d980535737e94f1b95f093c7fa514b9
lasthm <lasthm@qq.com>
2023-06-28 09:42:30 +0800

    Update GaugauDownloader.js

--- GaugauDownloader.js
+++ GaugauDownloader.js
@@ -1,76 +1,71 @@
 // ==UserScript==
 // @name         GaugauDownloader
 // @namespace    https://github.com/Timesient/manga-download-scripts
-// @version      0.4
+// @version      0.5
 // @license      GPL-3.0
 // @author       Timesient
-// @description  Manga downloader for specific for Speedbinb reader
-// @icon         https://gammaplus.takeshobo.co.jp/img/common/favicon.ico
-// @homepageURL  https://greasyfork.org/zh-CN/scripts/451879-speedbinbdownloader
+// @description  Manga downloader for booklive.jp
+// @icon         https://booklive.jp/favicon.ico
+// @homepageURL  https://greasyfork.org/zh-CN/scripts/452562-booklivedownloader
 // @supportURL   https://github.com/Timesient/manga-download-scripts/issues
 // @match        https://*.futabanet.jp/*
 // @require      https://unpkg.com/axios@0.27.2/dist/axios.min.js
 // @require      https://unpkg.com/jszip@3.7.1/dist/jszip.min.js
 // @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
 // @require      https://greasyfork.org/scripts/451810-imagedownloaderlib/code/ImageDownloaderLib.js?version=1129512
+// @require      https://greasyfork.org/scripts/456423-speedreadertools/code/SpeedReaderTools.js?version=1128886
 // @grant        GM_info
 // ==/UserScript==
 
-(async function (axios, JSZip, saveAs, ImageDownloader) {
+(async function(axios, JSZip, saveAs, ImageDownloader, SpeedReaderTools) {
   'use strict';
 
-  const params = new URL(window.location.href).searchParams;
-
-  // get base URL
+  // collect essential params
   const href = window.location.href;
   const baseURL = /futabanet\.jp/.test(href) ? href.split('?')[0].trim() : href;
-
-  // get cid
   const cid = baseURL.split('jp/')[1].trim();
-  if (!cid) return;
-
-  // get 'u' params and combine them
-  const uParams = Array(10).fill().map((_, index) => {
-    const param = params.get(`u${index}`);
-    return param ? `&u${index}=${param}` : '';
-  }).join('');
+  const randomString = SpeedReaderTools.generateRandomString32(cid);
 
   // generate config data
-  const randomString = SpeedReaderTools.generateRandomString32(cid);
-  const config = await new Promise(async resolve => {
-    const result = {};
-    while (!result.p) { // try until getting data successfully
-      await axios({
-        method: 'GET',
-        url: `https://reader.futabanet.jp/sws/bibGetCntntInfo.php?cid=${cid}&dmytime=${Date.now()}&k=${randomString}${uParams}`
-      }).then(res => {
-        const data = res.data.items[0];
-        if (data.p) {
-          result.title = data.Title;
-          result.contentServer = data.ContentsServer;
-          result.ctbl = SpeedReaderTools.getDecryptedTable(cid, randomString, data.ctbl);
-          result.ptbl = SpeedReaderTools.getDecryptedTable(cid, randomString, data.ptbl);
-          result.p = data.p;
-          resolve(result);
-        }
-      });
+  const config = await axios({
+    method: 'GET',
+    url: `https://reader.futabanet.jp/sws/bibGetCntntInfo.php?cid=${cid}&dmytime=${Date.now()}&k=${randomString}`
+  }).then(res => {
+    const data = res.data.items[0];
+    return {
+      title: data.Title,
+      contentServer: data.ContentsServer,
+      ctbl: SpeedReaderTools.getDecryptedTable(cid, randomString, data.ctbl),
+      ptbl: SpeedReaderTools.getDecryptedTable(cid, randomString, data.ptbl),
+      p: data.p
     }
   });
-  
-  // get data of image files
-  const files = await axios({
-    method: 'GET',
-    url: `${config.contentServer}/sbcGetCntnt.php?cid=${cid}&p=${config.p}&dmytime=${Date.now()}${uParams}`
-  }).then(res => {
-    const matchResult = res.data.ttx.matchAll(/(?<filename>(pages|images)\/[a-zA-Z0-9_]*.jpg)[^A-Z]*orgwidth="(?<width>\d*)" orgheight="(?<height>\d*)"/gm);
-    const result = Array.from(matchResult).map(match => ({
-      filename: match.groups.filename,
-      width: match.groups.width,
-      height: match.groups.height,
-      src: `${config.contentServer}/sbcGetImg.php?cid=${cid}&src=${match.groups.filename}&p=${config.p}&q=1`
+
+  // check if trial or not
+  const isTrial = config.contentServer.includes('trial');
+
+  // collect encrypted data of files
+  const contentData = await axios.get(
+    isTrial
+    ? `${config.contentServer}/content.js`
+    : `${config.contentServer}/sbcGetCntnt.php?cid=${cid}&p=${config.p}`
+  ).then(res => res.data);
+
+  // generate data of image files
+  const files = (() => {
+    const result = Array.from(
+      isTrial
+      ? contentData.matchAll(/(pages\\\/[a-zA-Z0-9_]*.jpg)[^A-Z]*orgwidth=\\\"(\d*)\\\" orgheight=\\\"(\d*)\\\"/gm)
+      : contentData.ttx.matchAll(/(pages\/[a-zA-Z0-9_]*.jpg)[^A-Z]*orgwidth="(\d*)" orgheight="(\d*)"/gm)
+    ).map(match => ({
+      filename: isTrial ? match[1].replace('\\', '') : match[1],
+      width: parseInt(match[2]),
+      height: parseInt(match[3]),
+      src: isTrial ? `${config.contentServer}/${match[1].replace('\\', '')}/M_L.jpg` : `${config.contentServer}/sbcGetImg.php?cid=${cid}&src=${encodeURIComponent(match[1])}&p=${config.p}`
     }));
+
     return result.slice(0, result.length / 2);
-  });
+  })();
 
   // setup ImageDownloader
   ImageDownloader.init({
@@ -92,15 +87,7 @@
   // get promise of decrypted image
   function getDecryptedImage(file) {
     return new Promise(async resolve => {
-      const imageArrayBuffer = await new Promise(resolve => {
-        GM_xmlhttpRequest({
-          method: 'GET',
-          url: file.src,
-          responseType: 'arraybuffer',
-          onload: res => resolve(res.response)
-        });
-      });
-
+      const imageArrayBuffer = await axios.get(file.src, { responseType: 'arraybuffer' }).then(res => res.data);
       const image = document.createElement('img');
       image.src = 'data:image/jpg;base64,' + window.btoa(new Uint8Array(imageArrayBuffer).reduce((data, byte) => data + String.fromCharCode(byte), ''));
       image.onload = function () {
@@ -118,6 +105,35 @@
         // draw pieces on correct position
         for (const { srcX, srcY, destX, destY, width, height } of coords) {
           ctx.drawImage(this, srcX, srcY, width, height, destX, destY, width, height);
+        }
+
+        // if trial, clear those transparent pixel
+        if (isTrial) {
+          let originalWidth;
+          for (let w = canvas.width; w >= 0; w--) {
+            const px = canvas.getContext('2d').getImageData(w, 0, 1, 1);
+            if (!Array.from(px.data).every(data => data === 0)) {
+              originalWidth = w;
+              break;
+            }
+          }
+
+          let originalHeight;
+          for (let h = canvas.height; h >= 0; h--) {
+            const px = canvas.getContext('2d').getImageData(0, h, 1, 1);
+            if (!Array.from(px.data).every(data => data === 0)) {
+              originalHeight = h;
+              break;
+            }
+          }
+
+          canvas.width = originalWidth;
+          canvas.height = originalHeight;
+          ctx = canvas.getContext('2d');
+
+          for (const { srcX, srcY, destX, destY, width, height } of coords) {
+            ctx.drawImage(this, srcX, srcY, width, height, destX, destY, width, height);
+          }
         }
 
         canvas.toBlob(resolve);
